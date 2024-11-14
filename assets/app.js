6d902cb41654e056838e1836e78e00d12894899c
Erwann Glath <erwannglath@gmail.com>
2024-11-14 23:11:09 +0100

    Ajout du carousel des derniers arrivages - carousel non terminé

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,29 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener('DOMContentLoaded', () => {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.item-carousel');
+    const totalSlides = slides.length;
+    const carouselImages = document.querySelector('.carousel-images');
+
+    function updateSlidePosition() {
+        const offset = -currentSlide * 100;
+        carouselImages.style.transform = `translateX(${offset}%)`;
+    }
+
+    function changeSlide(direction) {
+        currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+        updateSlidePosition();
+    }
+
+    // Ajoute les écouteurs d'événements pour les boutons
+    const prevButton = document.querySelector('.carousel-button.prev');
+    const nextButton = document.querySelector('.carousel-button.next');
+
+    prevButton.addEventListener('click', () => changeSlide(-1));
+    nextButton.addEventListener('click', () => changeSlide(1));
+});
+
+
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +12,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class FrontController extends AbstractController {
 
     #[Route('/', name:'index')]
-    function index(Request $request): Response {
-        return $this->render('front/index.html.twig');
+    function index(Request $request, EntityManagerInterface $entityManager): Response {
+        // Récupère les 3 derniers articles
+        $articles = $entityManager->getRepository(Article::class)
+            ->findBy([], ['createdAt' => 'DESC'], 3);
+
+        return $this->render('front/index.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 }
